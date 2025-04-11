b830a4ae0db0527495a1ae8b7006cc7144c341d6
AmmarMubashir <muhammadammarmubashir@gmail.com>
2025-04-11 20:43:47 +0500

    endpoints modified

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.post("/api/login", async (req, res) => {
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
-    res.json({ token });
+    res.json({ token, message: "Iser login successfully" });
   } catch (err) {
     res.status(500).json({ message: "Server error", error: err.message });
   }
